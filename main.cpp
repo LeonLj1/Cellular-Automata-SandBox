582fa9442382127245d2393a9c097c9f80d2a98f
Leon <leonljajic@gmail.com>
2026-04-28 16:08:53 +0200

    Fixed water spread , sand sinking in water; Added fire and smoke

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 #include <algorithm>
-#include <vector>
+
+using namespace std;
 
 const int WIDTH=900;
 const int HEIGHT=600;
@@ -14,38 +15,45 @@ const int GRID_HEIGHT=HEIGHT/CELL_SIZE;
 enum Cell{
     EMPTY,
     SAND,
-    WATER
+    WATER,
+    STONE,
+    WOOD,
+    FIRE,
+    SMOKE
 };
 
-std::vector<Cell> grid(GRID_WIDTH*GRID_HEIGHT,EMPTY);
-
-inline int idx(int x,int y){
-    return y*GRID_WIDTH+x;
-}
+int grid[GRID_WIDTH][GRID_HEIGHT];
 
 void updateSand(int x,int y){
     if(y+1>=GRID_HEIGHT) return;
 
-    if(grid[idx(x,y+1)]==EMPTY){
-        std::swap(grid[idx(x,y)],grid[idx(x,y+1)]);
+    int sandShift=!(rand()%15);
+
+    if(grid[x][y+1]==EMPTY || (sandShift && grid[x][y+1]==WATER)){
+        swap(grid[x][y],grid[x][y+1]);
     }else{
         
         int left=rand()%2;
 
+        sandShift=!(rand()%15);
+
         if(left){
-            if(x>0 && grid[idx(x-1,y+1)]==EMPTY){
-                std::swap(grid[idx(x,y)],grid[idx(x-1,y+1)]);
+            if((x>0 && grid[x-1][y+1]==EMPTY) || (x>0 && sandShift && grid[x-1][y+1]==WATER)){
+                swap(grid[x][y],grid[x-1][y+1]);
             }else{
-                if(x<GRID_WIDTH-1 && grid[idx(x+1,y+1)]==EMPTY){
-                    std::swap(grid[idx(x,y)],grid[idx(x+1,y+1)]);
+                sandShift=!(rand()%15);
+                if((x<GRID_WIDTH-1 && grid[x+1][y+1]==EMPTY) || (x<GRID_WIDTH-1 && sandShift && grid[x+1][y+1]==WATER)){
+                    swap(grid[x][y],grid[x+1][y+1]);
                 }
             }
         }else{
-            if(x<GRID_WIDTH-1 && grid[idx(x+1,y+1)]==EMPTY){
-                std::swap(grid[idx(x,y)],grid[idx(x+1,y+1)]);
+            sandShift=!(rand()%15);
+            if((x<GRID_WIDTH-1 && grid[x+1][y+1]==EMPTY) || (x<GRID_WIDTH-1 && sandShift && grid[x+1][y+1]==WATER)){
+                swap(grid[x][y],grid[x+1][y+1]);
             }else{
-                if(x>0 && grid[idx(x-1,y+1)]==EMPTY){
-                    std::swap(grid[idx(x,y)],grid[idx(x-1,y+1)]);
+                sandShift=!(rand()%15);
+                if((x>0 && grid[x-1][y+1]==EMPTY) || (x>0 && sandShift && grid[x-1][y+1]==WATER)){
+                    swap(grid[x][y],grid[x-1][y+1]);
                 }
             }
         }
@@ -55,36 +63,115 @@ void updateSand(int x,int y){
 void updateWater(int x,int y){
     if(y+1 >= GRID_HEIGHT) return;
 
-    if(grid[idx(x,y+1)]==EMPTY){
-        std::swap(grid[idx(x,y)],grid[idx(x,y+1)]);
+    if(grid[x][y+1]==EMPTY){
+        swap(grid[x][y],grid[x][y+1]);
     }else{
+
+        int dis=rand()%3+1;
+
+        //if(grid[x+dis-1][y]!=EMPTY || grid[x+dis-2][y]!=EMPTY || grid[x-dis-1][y]!=EMPTY || grid[x-dis-2][y]!=EMPTY) return;
+
+        if((x>dis-1 && grid[x-dis][y]==EMPTY && grid[x+1][y]!=EMPTY) || x+1>=GRID_WIDTH){
+            swap(grid[x][y],grid[x-dis][y]);
+            return;
+        }else{
+            if((x<GRID_WIDTH-dis && grid[x+dis][y]==EMPTY && grid[x-1][y]!=EMPTY) || x-1<=0){
+                swap(grid[x][y],grid[x+dis][y]);
+                return;
+            } 
+        }
+
         int left=rand()%2;
 
         if(left){
-            if(x>0 && grid[idx(x-1,y)]==EMPTY){
-                std::swap(grid[idx(x,y)],grid[idx(x-1,y)]);
+            if(x>dis-1 && grid[x-dis][y]==EMPTY){
+                swap(grid[x][y],grid[x-dis][y]);
             }else{
-                if(x<GRID_WIDTH-1 && grid[idx(x+1,y)]==EMPTY){
-                    std::swap(grid[idx(x,y)],grid[idx(x+1,y)]);
+                if(x<GRID_WIDTH-dis && grid[x+dis][y]==EMPTY){
+                    swap(grid[x][y],grid[x+dis][y]);
                 }
             }
         }else{
-            if(x<GRID_WIDTH-1 && grid[idx(x+1,y)]==EMPTY){
-                std::swap(grid[idx(x,y)],grid[idx(x+1,y)]);
+            if(x<GRID_WIDTH-dis && grid[x+dis][y]==EMPTY){
+                swap(grid[x][y],grid[x+dis][y]);
             }else{
-                if(x>0 && grid[idx(x-1,y)]==EMPTY){
-                    std::swap(grid[idx(x,y)],grid[idx(x-1,y)]);
+                if(x>dis-1 && grid[x-dis][y]==EMPTY){
+                    swap(grid[x][y],grid[x-dis][y]);
                 }
-            }
-
+           }
         }
     }
+
+}
+
+void updateFire(int x,int y){
+
+    int fireDesappear=!(rand()%10);
+
+    int toSmoke=!(rand()%100);
+
+    if(fireDesappear){
+        if(toSmoke){
+            grid[x][y]=SMOKE;
+        }else{
+            grid[x][y]=EMPTY;
+        }
+        return;
+    }
+
+    if(y-1<=0) return;
+
+    int spread=!(rand()%20);
+
+    if(y-1>0 && grid[x][y-1]==EMPTY && spread){
+        grid[x][y-1]=FIRE;
+    }else{
+        spread=!(rand()%20);
+        if(x-1>0 && y-1>0 && grid[x-1][y-1]==EMPTY && spread){
+            grid[x-1][y-1]=FIRE;
+        }else{
+            spread=!(rand()%20);
+            if(x+1<GRID_WIDTH-1 && y-1>0 && grid[x+1][y-1]==EMPTY && spread){
+                grid[x+1][y-1]=FIRE;
+            }
+        }
+    }
+
+}
+
+void updateSmoke(int x,int y){
+
+    int desappear=!(rand()%100);
+
+    if(desappear){
+        grid[x][y]=EMPTY;
+        return;
+    }
+
+    if(y-1<=0) return;
+
+    int toMove=!(rand()%5);
+
+    if(y-1>0 && grid[x][y-1]==EMPTY && toMove){
+        swap(grid[x][y],grid[x][y-1]);
+    }else{
+        if(x-1>0 && grid[x-1][y-1]==EMPTY && toMove){
+            swap(grid[x][y], grid[x-1][y-1]);
+        }else{
+            if(x<GRID_WIDTH-1 && grid[x+1][y-1]==EMPTY && toMove){
+                swap(grid[x][y], grid[x+1][y-1]);
+            }
+        }
+    }
+}
+
+void updateWood(int x, int y){
     
 }
 
 int main(){
     if(SDL_Init(SDL_INIT_VIDEO)!=0){
-        std::cout<<"SDL intit failed!!!\n";
+        cout<<"SDL intit failed!!!\n";
         return -1;
     }
 
@@ -111,14 +198,7 @@ int main(){
             if(event.type == SDL_QUIT) running=0;
             else{
                 if(event.type == SDL_TEXTINPUT){
-                    ch=event.text.text[0];
-
-                    switch(ch-'0'){
-                        case 0: particle=0; break;
-                        case 1: particle=1; break;
-                        case 2: particle=2; break;
-                        default: particle=1; break;
-                    }
+                    ch=event.text.text[0];   
                 }
             }
         }
@@ -131,21 +211,38 @@ int main(){
         if(mouse & SDL_BUTTON(SDL_BUTTON_LEFT)){
             if(gx>=0 && gx<GRID_WIDTH && gy>=0 && gy<GRID_HEIGHT){
                 switch(ch-'0'){
-                    case 0: grid[idx(gx,gy)]=EMPTY; break;
-                    case 1: grid[idx(gx,gy)]=SAND; break;
-                    case 2: grid[idx(gx,gy)]=WATER; break;
-                    default: grid[idx(gx,gy)]=SAND; break;
-                }
+                    case 0: grid[gx][gy]=EMPTY; break;
+                    case 1: grid[gx][gy]=SAND; break;
+                    case 2: grid[gx][gy]=WATER; break;
+                    case 3: grid[gx][gy]=STONE; break;
+                    case 4: grid[gx][gy]=WOOD; break;
+                    case 5: grid[gx][gy]=FIRE; break;
+                    case 6: grid[gx][gy]=SMOKE; break;
+                    default: grid[gx][gy]=SAND; break;
+                 }
             }
         }
 
         for(int y=GRID_HEIGHT-1;y>=0;y--){
-            for(int x=0;x<GRID_WIDTH;x++){
-                if(grid[idx(x,y)]==SAND){
+
+            int leftToRight = rand() % 2;
+
+            for(int i=0;i<GRID_WIDTH;i++){
+                int x = leftToRight ? i : GRID_WIDTH-1-i;
+
+                if(grid[x][y]==SAND){
                     updateSand(x,y);
                 }else{
-                    if(grid[idx(x,y)]==WATER){
+                    if(grid[x][y]==WATER){
                         updateWater(x,y);
+                    }else{
+                        if(grid[x][y]==FIRE){
+                            updateFire(x,y);
+                        }else{
+                            if(grid[x][y]==SMOKE){
+                                updateSmoke(x,y);
+                            }
+                        }
                     }
                 }
             }
@@ -156,7 +253,7 @@ int main(){
 
         for(int y=0;y<GRID_HEIGHT;y++){
             for(int x=0;x<GRID_WIDTH;x++){
-                if(grid[idx(x,y)]==SAND){
+                if(grid[x][y]==SAND){
                     SDL_Rect r = {
                         x*CELL_SIZE,
                         y*CELL_SIZE,
@@ -166,7 +263,7 @@ int main(){
                     SDL_SetRenderDrawColor(renderer, 194, 178, 128, 255);
                     SDL_RenderFillRect(renderer, &r);
                 }else{
-                    if(grid[idx(x,y)]==WATER){
+                    if(grid[x][y]==WATER){
                         SDL_Rect r = {
                             x*CELL_SIZE,
                             y*CELL_SIZE,
@@ -176,19 +273,64 @@ int main(){
 
                         SDL_SetRenderDrawColor(renderer, 0, 105, 148, 255);
                         SDL_RenderFillRect(renderer, &r);
+                    }else{
+                        if(grid[x][y]==STONE){
+                            SDL_Rect r = {
+                                x*CELL_SIZE,
+                                y*CELL_SIZE,
+                                CELL_SIZE,
+                                CELL_SIZE
+                            };
+
+                            SDL_SetRenderDrawColor(renderer, 128, 128, 128, 255);
+                            SDL_RenderFillRect(renderer, &r);
+                        }else{
+                            if(grid[x][y]==WOOD){
+                                SDL_Rect r={
+                                    x*CELL_SIZE,
+                                    y*CELL_SIZE,
+                                    CELL_SIZE,
+                                    CELL_SIZE
+                                };
+                                SDL_SetRenderDrawColor(renderer, 156, 119, 64, 255);
+                                SDL_RenderFillRect(renderer, &r);
+                            }else{
+                                if(grid[x][y]==FIRE){
+                                    SDL_Rect r={
+                                        x*CELL_SIZE,
+                                        y*CELL_SIZE,
+                                        CELL_SIZE,
+                                        CELL_SIZE
+                                    };
+                                    SDL_SetRenderDrawColor(renderer, 200, 0, 0, 255);
+                                    SDL_RenderFillRect(renderer, &r);
+                                }else{
+                                    if(grid[x][y]==SMOKE){
+                                        SDL_Rect r={
+                                            x*CELL_SIZE,
+                                            y*CELL_SIZE,
+                                            CELL_SIZE,
+                                            CELL_SIZE
+                                        };
+                                        SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+                                        SDL_RenderFillRect(renderer, &r);
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
 
         SDL_RenderPresent(renderer);
-        SDL_Delay(16);
+        SDL_Delay(2);
 
     }
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
-    SDL_Quit;
+    SDL_Quit();
 
     return 0;
 }
